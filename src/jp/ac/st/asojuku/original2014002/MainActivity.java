b562123782ec58cb6391fc1bc44ef2e9be9b795b
student <student@PC111S10.fgakusei.local>
2014-06-12 15:08:08 +0900

    6/12

--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -2,12 +2,18 @@ package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +25,25 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
+
 		Button btnmainte = (Button)findViewById(R.id.btnmainte);
 		btnmainte.setOnClickListener(this);
+
+		Button btntouroku = (Button)findViewById(R.id.btntouroku);
+		btntouroku.setOnClickListener(this);
+
+		Button btncheck = (Button)findViewById(R.id.btncheck);
+		btncheck.setOnClickListener(this);
+
+		//クラスのフィールド変数がNULLなら、データベース空間オープン
+		if(sdb == null) {
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		} try {
+			sdb = helper.getWritableDatabase();
+		} catch(SQLiteException e) {
+			//異常終了
+			return;
+		}
 	}
 
 	@Override
@@ -29,16 +52,26 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		Intent intent = null;
 		switch(v.getId()){
 			case R.id.btnmainte:
-
 				intent = new Intent(MainActivity.this, MaintenanceActivity.class);
 				startActivity(intent);
 				break;
 			case R.id.btntouroku:
+				EditText etv = (EditText)findViewById(R.id.edtMsg);
+				String inputMsg = etv.getText().toString();
 
+				//inputMsgがnullでない、かつ、空でない場合のみif文内を実行
+				if(inputMsg!=null && !inputMsg.isEmpty()) {
+					//MySQLiteOpenHelperのインサートメソッドを呼び出し
+					helper.insertHitokoto(sdb, inputMsg);
+				}
+				etv.setText("");
 				break;
 			case R.id.btncheck:
+				//MySQLiteOpenHelperのセレクト一言メソッドを呼び出して一言をランダムに取得
+				String strHitokoto = helper.selectRandomHitokoto(sdb);
 
 				intent = new Intent(MainActivity.this, HitokotoActivity.class);
+				intent.putExtra("hitokoto", strHitokoto);
 				startActivity(intent);
 				break;
 		}
--- src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
+++ src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
@@ -1,8 +1,11 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 
@@ -30,6 +33,50 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 		// TODO 自動生成されたメソッド・スタブ
 		db.execSQL("drop table Hitokoto");
 		onCreate(db);
+	}
+
+	/**
+	 * 引数のフレーズをHitokotoテーブルにインサートするプライベートメソッド
+	 * @param SQLiteDatabase インサート先のDBのインスタンス変数
+	 * @param inputMsg インサートするメッセージ
+	 */
+	public void insertHitokoto(SQLiteDatabase db, String inputMsg){
+
+		String sqlstr = "insert into Hitokoto (phrase) Values(' " + inputMsg + " '); ";
+			try {
+				//トランザクション開始
+				db.beginTransaction();
+				db.execSQL(sqlstr);
+				//トランザクション成功
+				db.setTransactionSuccessful();
+			} catch (SQLException e) {
+				Log.e("ERROR", e.toString());
+			} finally {
+				//トランザクション終了
+				db.endTransaction();
+			}
+		return;
+	}
+
+	public String selectRandomHitokoto(SQLiteDatabase db) {
+
+		String rtString = null;
+
+		String sqlstr = " SELECT _id, phrase FROM Hitokoto ORDER BY RANDOM(); ";
+			try {
+				SQLiteCursor cursor = (SQLiteCursor)db.rawQuery(sqlstr, null);
+				if(cursor.getCount()!=0) {
+					//カーソル開始位置を先頭にする
+					cursor.moveToFirst();
+					rtString = cursor.getString(1);
+				}
+				cursor.close();
+			} catch (SQLException e) {
+				Log.e("ERROR", e.toString());
+			} finally {
+				//既にカーソルもcloseしてあるので、何もしない
+			}
+		return rtString;
 	}
 
 }
